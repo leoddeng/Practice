bfcb9d72f71eecfcee1257946cf57b6f9e1a5fd2
DY <3233891353@qq.com>
2025-03-21 11:57:09 +0800

    feat: 添加权限组件页面

---
+++ src/pages/AuthorizationComponent/Authorization.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(['permission'])
+const roles = [
+  'admin', 'editor', 'guest'
+]
+</script>
+
+<template>
+  <template v-if="roles.includes(permission)">
+    <slot name="extra"></slot>
+    <slot></slot>
+  </template>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/AuthorizationComponent/index.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import Authorization from './Authorization.vue';
+const selected = ref('admin');
+</script>
+
+<template>
+  <div class="authorization">
+    <div class="selector">
+      <label for="role-select">Choose roles:</label>
+      <select v-model="selected" id="role-s">
+        <option value="admin">admin</option>
+        <option value="guest">guest</option>
+        <option value="editor">editor</option>
+      </select>
+    </div>
+    <div>
+      <Authorization :permission="selected">
+        {{ selected }}
+      </Authorization>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.selector {
+  display: flex;
+  flex-direction: column;
+}
+
+label {
+  font-family: sans-serif;
+  font-size: 1rem;
+  padding-right: 10px;
+}
+
+select {
+  font-size: 0.9rem;
+  padding: 2px 5px;
+}
+</style>
